0917bf7a659f91c57a00f0d566c393db3822318c
MrZou <382066178@qq.com>
2018-03-13 16:12:30 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,14 @@
   import MHeader from 'components/m-header/m-header'
   import VTab from 'components/tab/tab'
   import Player from 'components/player/player'
-
+  /**
+   * 为了防止,每次点击router-link触发，路由切换时，
+   * router-view中的组件都会重新渲染并请求数据，造成切换时页面晃动，引响性能和体验
+   * 可在router-view外层包一层keep-alive，这样组件就会保存在内存中，切换时直接从内存读取，而避免重新渲染
+   * <keep-alive>
+   *   <router-view></router-view>
+   * </keep-alive>
+   * */
   export default {
     components: {
       MHeader,
