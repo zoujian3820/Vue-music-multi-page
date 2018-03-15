52c444b41eb0c02e137055099c2b98889eea4a1c
MrZou <382066178@qq.com>
2018-03-15 15:26:20 +0800

    update

--- src/store/actions.js
+++ src/store/actions.js
@@ -11,13 +11,32 @@ function findIndex(list, song) {
   })
 }
 
+/**
+ * action传参第一个参数有两种方式：
+ * 1、直接用es6语法传{commit,state}
+ * 2、传context对象,然后：context.commit   context.state
+ *
+ * action: 用来提交mutation，是异步提交，可提交一个也可同时提交多个mutation
+ *
+ * const actions = {
+ *    //2种书写方式
+ *  addplus(context){ //可以理解为代表了整个的上下文
+ *    context.commit('add',{a:10})
+ *  },
+ *  subplus({commit}){
+ *    commit('sub');
+ *   }
+ * };
+ *
+ * */
+
 export const selectPlay = function ({commit,state}, {list, index}) {
   commit(types.SET_SEQUENCELIST, list)
   if (state.mode === playMode.random) {
     const radomList = shuffle(list)
     commit(types.SET_PLAYLIST, radomList)
     index = findIndex(radomList, list[index])
-  }else{
+  } else {
     commit(types.SET_PLAYLIST, list)
   }
   commit(types.SET_CURRENTINDEX, index)
--- src/store/index.js
+++ src/store/index.js
@@ -14,7 +14,7 @@ Vue.use(Vuex)
 const debug = process.env.NODE_ENV !== 'production'
 
 export default new Vuex.Store({
-  actions,    //'src/component/music-list/music-list.vue' 异步修改数据，Action 提交的是 mutation
+  actions,    //'src/component/music-list/music-list.vue' 异步修改数据，Action 提交的是 mutation,可以是多个mutation
   getters,    //'src/component/singer-detail/singer-detail.vue' 获取vuex中的数据，在computed中调用computed:{...mapGetters(['singer'])},在组件中直接调用this.singer, 如： created() {console.log('vuex-singer:',this.singer)}
   state,      //'src/store/state.js' 初始数据结构
   mutations,  //'src/component/singer/singer.vue' 修改数据，在methods中调用methods:{...mapMutations({setSinger: 'SET_SINGER'})},在组件中直接调用this.setSinger(item)
